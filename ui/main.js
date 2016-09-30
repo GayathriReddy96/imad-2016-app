7fc7831ad9b8ba7839603c46dcc28f2ce31f47ba
R Gayathri Reddy <gayatri96reddy@gmail.com>
2016-09-30 15:36:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+//counter
+var button=document.getElementbyId('counter');
+var counter=0;
+button.onClick=function(){
+    counter=counter+1;
+    var span=document.getElementbyId('count');
+    span.InnerHTML=counter.toString();
+};
